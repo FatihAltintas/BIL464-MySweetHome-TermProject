79c1f68e090c2ed0588f586c36010ac3ca96dbd3
osmanovel50-art <osmanovel50@gmail.com>
2025-12-03 16:05:42 +0300

    Update Camera.h

--- REG 6-7.h/REQ 6-7/include/Camera.h
+++ REG 6-7.h/REQ 6-7/include/Camera.h
@@ -1,1 +1,20 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "Device.h"
+
+// Guvenlik kamerasini temsil eden sinif
+class Camera : public Device {
+public:
+    Camera(const std::string& n, int i)
+        : Device(n, i) {}
+
+    virtual Device* clone() const override {
+        return new Camera(*this);
+    }
+
+    // Ileride FPS, aci, cozunurluk vb. ozellikler eklenebilir
+};
+
+#endif
 
