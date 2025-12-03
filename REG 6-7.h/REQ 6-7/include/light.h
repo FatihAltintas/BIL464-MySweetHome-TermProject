66934f0ebaabf131d48c6665f2bdf731ca34bfe2
osmanovel50-art <osmanovel50@gmail.com>
2025-12-03 16:04:08 +0300

    Implement Light class in light.h
    Added Light class to represent a light device, inheriting from Device.

--- REG 6-7.h/REQ 6-7/include/light.h
+++ REG 6-7.h/REQ 6-7/include/light.h
@@ -1,1 +1,23 @@
+#ifndef LIGHT_H
+#define LIGHT_H
+
+#include "Device.h"
+
+// Light cihazini temsil eden sinif
+class Light : public Device {
+public:
+    // Kurucu fonksiyon: isim ve ID alir, base class'a gecer
+    Light(const std::string& n, int i)
+        : Device(n, i) {}
+
+    // Prototype pattern: bu nesnenin kopyasini uretir
+    virtual Device* clone() const override {
+        return new Light(*this);
+    }
+
+    // Istersek ileride Light'a ozel davranis ekleyebiliriz
+    // (su an icin Device'in powerOn/powerOff fonksiyonlarini kullaniyoruz)
+};
+
+#endif
 
