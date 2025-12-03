05011848191798d7120c1b1d328c08b6c32d0ba8
osmanovel50-art <osmanovel50@gmail.com>
2025-12-03 16:06:27 +0300

    Add Alarm class to represent alarm devices

--- REG 6-7.h/REQ 6-7/include/Alarm.h
+++ REG 6-7.h/REQ 6-7/include/Alarm.h
@@ -1,1 +1,26 @@
+#ifndef ALARM_H
+#define ALARM_H
+
+#include "Device.h"
+
+// Alarm cihazini temsil eden sinif
+class Alarm : public Device {
+public:
+    // Alarm varsayilan olarak ON baslayabilir
+    Alarm(const std::string& n, int i)
+        : Device(n, i) {
+        isActive = true;   // Sistem acilir acilmaz aktif
+    }
+
+    virtual Device* clone() const override {
+        return new Alarm(*this);
+    }
+
+    // Alarm kullanici tarafindan kapatilamaz (REQ6 ozel durumu)
+    virtual void powerOff() override {
+        std::cout << name << " cannot be turned OFF by user!" << std::endl;
+    }
+};
+
+#endif
 
