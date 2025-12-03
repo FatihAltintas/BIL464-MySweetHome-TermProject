af5c98233ae19a7c3aba20e88cc6a82149bef71b
osmanovel50-art <osmanovel50@gmail.com>
2025-12-03 16:36:10 +0300

    Refactor TV class constructor and operate method

--- REG 6-7.h/REQ 6-7/include/Tv.h
+++ REG 6-7.h/REQ 6-7/include/Tv.h
@@ -3,18 +3,19 @@
 
 #include "Device.h"
 
-// TV cihazini temsil eden sinif
+// TV cihazını temsil eden sınıf
 class TV : public Device {
 public:
-    TV(const std::string& n, int i)
-        : Device(n, i) {}
+    TV(const string& n)
+        : Device(n) {}
 
     virtual Device* clone() const override {
         return new TV(*this);
     }
 
-    // Ileride kanal, ses vb. ozellikler eklenebilir
+    virtual void operate() override {
+        cout << "[TV] " << name << " is showing content." << endl;
+    }
 };
 
 #endif
-
