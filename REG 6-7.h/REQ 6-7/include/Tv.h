03918438fdf4a08c6c90dc6bfe7286780505da12
osmanovel50-art <osmanovel50@gmail.com>
2025-12-03 16:04:54 +0300

    Create TV class in Tv.h
    Add TV class to represent a television device.

--- REG 6-7.h/REQ 6-7/include/Tv.h
+++ REG 6-7.h/REQ 6-7/include/Tv.h
@@ -1,1 +1,20 @@
+#ifndef TV_H
+#define TV_H
+
+#include "Device.h"
+
+// TV cihazini temsil eden sinif
+class TV : public Device {
+public:
+    TV(const std::string& n, int i)
+        : Device(n, i) {}
+
+    virtual Device* clone() const override {
+        return new TV(*this);
+    }
+
+    // Ileride kanal, ses vb. ozellikler eklenebilir
+};
+
+#endif
 
