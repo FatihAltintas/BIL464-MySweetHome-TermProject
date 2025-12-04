1f89585fbf1147fbe782e8ee64fd9731720d3966
Erhannn <xxniaxlife@hotmail.com>
2025-12-04 18:58:53 +0300

    Merge oncesi son durum kaydedildi

--- include/Device.h
+++ include/Device.h
@@ -5,70 +5,88 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 class Device;
+
 
 class IObserver {
 public:
     virtual ~IObserver() {}
+    
     virtual void update(Device* device) = 0;
 };
 
+
 enum DeviceState { ACTIVE, INACTIVE, FAILED };
+
 
 class Device {
 protected:
-    std::string name;
+    string name;
     int id;
-    DeviceState state;
-    std::vector<IObserver*> observers;
+    DeviceState state; 
+
+    
+    vector<IObserver*> observers;
 
 public:
-    Device(const std::string& n) : name(n), id(0), state(INACTIVE) {}
+    // Constructor
+    Device(const string& n) : name(n), id(0), state(INACTIVE) {}
     virtual ~Device() {}
 
+   
     virtual Device* clone() const = 0;
+
+    
     virtual void operate() = 0;
 
+    
     virtual void copyConfigFrom(const Device* other) {
         if (other) {
             this->name = other->name;
-            std::cout << "[LOG] Config copied from " << other->name << std::endl;
+           
+            cout << "[LOG] Config copied from " << other->name << endl;
         }
     }
 
     virtual void powerOn() {
         if (state != FAILED) {
             state = ACTIVE;
-            std::cout << name << " ACILDI." << std::endl;
+            cout << name << " is ON." << endl;
             notifyObservers();
         }
         else {
-            std::cout << name << " FAILEd. Cannot power on." << std::endl;
+            cout << name << " is FAILED. Cannot power on." << endl;
         }
     }
 
     virtual void powerOff() {
         if (state != FAILED) {
             state = INACTIVE;
-            std::cout << name << " KAPATILDI." << std::endl;
+            cout << name << " is OFF." << endl;
             notifyObservers();
         }
     }
 
+    
     void setState(DeviceState newState) {
         state = newState;
-        notifyObservers();
+        notifyObservers(); 
     }
 
     DeviceState getState() const { return state; }
-    std::string getName() const { return name; }
+    string getName() const { return name; }
     int getId() const { return id; }
     void setId(int newId) { id = newId; }
 
-    void attach(IObserver* observer) { observers.push_back(observer); }
+
+    void attach(IObserver* observer) {
+        observers.push_back(observer);
+    }
 
     void detach(IObserver* observer) {
-        for (std::vector<IObserver*>::iterator it = observers.begin(); it != observers.end(); ++it) {
+        for (vector<IObserver*>::iterator it = observers.begin(); it != observers.end(); ++it) {
             if (*it == observer) {
                 observers.erase(it);
                 break;
@@ -82,4 +100,5 @@ public:
         }
     }
 };
+
 #endif
