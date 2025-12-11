baf8b9d3a04ad1edc57ac0efeb07eeea45b0cac7
Erhan Bülbül <xxniaxlife@hotmail.com>
2025-12-11 20:17:53 +0300

    Update MSHSystem.h

--- include/MSHSystem.h
+++ include/MSHSystem.h
@@ -4,26 +4,41 @@
 #include "Device.h"
 #include "StateCaretaker.h"
 #include "HomeMemento.h"
+#include "ModeManager.h"
 #include <vector>
+#include <string>
 
 class MSHSystem {
 private:
     std::vector<Device*> devices;
     StateCaretaker* caretaker;
-    std::string currentMode;
+    ModeType currentMode;
 
 public:
     MSHSystem();
     ~MSHSystem();
 
+    // Helper
+    Device* getDevice(int id);
+
+    // Cihaz Yonetimi
     void addDevice(Device* d);
+    void removeDevice(int id); 
     
-    void duplicateDevice(int sourceDeviceId, int newId);
-
-    void changeMode(const std::string& newMode);
+    // REQ10: Prototype
+    void duplicateDevice(int index, int newId);
+    
+    // REQ11 & REQ7: Mod ve Durum Yonetimi
+    void changeMode(ModeType newMode);
+    
+    // REQ12: Undo
     void restorePreviousMode();
-
+    
+    // Listeleme
     void listDevices();
+
+    // REQ16: Taha'nin Acil Durum Senaryosu
+    void callFireStation();
 };
 
 #endif
