8ef9f61c4f31bbf6182eab1c474e47776314dadd
Erhan Bülbül <xxniaxlife@hotmail.com>
2025-12-18 09:59:34 +0300

    Update ChangeStateCommand.h

--- include/ChangeStateCommand.h
+++ include/ChangeStateCommand.h
@@ -1,16 +1,30 @@
 #ifndef CHANGESTATECOMMAND_H
 #define CHANGESTATECOMMAND_H
-
-#include "IMenuCommand.h"
-#include "MSHSystem.h"
+/*
+Bu dosya, kullanıcının sistemin durumunu (State/Mode) değiştirmesini
+ve Geri Al (Undo) işlemini yapmasını sağlayan komutun tanımıdır.
+Bu sınıf, Command Pattern'daki "Concrete Command" (Somut Komut) rolündedir.
+*/    
+#include "IMenuCommand.h" // Base Class (Interface) - Polimorfizm
+#include "MSHSystem.h" // Receiver (Alıcı) - İşlemi asıl yapacak olan sınıf
 #include <string>
 
-class ChangeStateCommand : public IMenuCommand {
+// Bu sınıfın görevi: Menüden "7" seçildiğinde devreye girmek ve
+// MSHSystem üzerindeki changeMode() veya restorePreviousMode() 
+//  fonksiyonlarını tetiklemektir.
+class ChangeStateCommand : public IMenuCommand {  
 private:
+
+//sistemin kopyası üzerinde değil, bellekteki
+// GERÇEK sistem üzerinde değişiklik yapmak istiyoruz.
     MSHSystem* system;
 
 public:
     ChangeStateCommand(MSHSystem* sys);
+
+    // bu bir alt sınıf. Eğer bir ChangeStateCommand nesnesini
+    // IMenuCommand* (Base) pointer'ı üzerinden silersek ve bu virtual olmazsa,
+    // sadece üst sınıf silinir, alt sınıf bellekte kalır (Memory Leak).
     virtual ~ChangeStateCommand();
 
     void execute();
@@ -18,3 +32,7 @@ public:
 };
 
 #endif
+
+// bu Command Pattern'a aittir. Rolü Concrete Command'dır 
+// (Somut Komut). Kullanıcının isteğini (State değiştirmek veya Geri almak) bir nesneye 
+// dönüştürür ve arka plandaki MSHSystem'e iletir."
